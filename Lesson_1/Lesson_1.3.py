dbb985593e07ddab20cb738701828912f83dc570
thetribalme <thetribaleng@gmail.com>
2023-05-22 18:50:26 +0200

    deleted list of numbers forming realisation with if added renamed variable tmp to ending_check (3/3)

--- Lesson_1/Lesson_1.3.py
+++ Lesson_1/Lesson_1.3.py
@@ -3,11 +3,26 @@
 экран отдельной строкой для каждого из чисел в интервале от 1 до 100
 '''
 
-list_till = 100    # до какого числа составлять список
+list_till = 30    # до какого числа составлять список
 word_root = " процент"
-number_list = []
-idx = 0
-# ты мне разрешила делать через словарь)
+exceptions = [11, 12, 13, 14]
+
+# realisation with if
+ov_list = [0, 5, 6, 7, 8, 9]    # если число оканчивается на это, то употребляется окончание "ов"
+a_list = [2, 3, 4]    # если на это - окончание "а". В остальных случаях окончание нулевое
+ov_ending = "ов"
+a_ending = "а"
+
+for number in range(list_till + 1):
+    ending_check = number % 10
+    if ending_check in ov_list or number in exceptions:
+        print(str(number) + word_root + ov_ending)
+    elif ending_check in a_list:
+        print(str(number) + word_root + a_ending)
+    else:
+        print(str(number) + word_root)
+
+# realisation with dictionary
 word_ending = {
     0: "ов",
     1: "",
@@ -21,9 +36,9 @@ word_ending = {
     9: "ов"
 }
 
-for idx in range(list_till):
-    number_list.append(idx+1)
-
-for idx in range(len(number_list)):
-    tmp = number_list[idx] % 10
-    print(str(number_list[idx]) + word_root + str(word_ending[tmp]))
+for number in range(list_till + 1):
+    ending_check = number % 10
+    if number in exceptions:
+        print(str(number) + word_root + str(word_ending[0]))
+    else:
+        print(str(number) + word_root + str(word_ending[ending_check]))

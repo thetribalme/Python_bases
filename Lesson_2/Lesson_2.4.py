8939d1b5def3d086509b81f5692dfec2c88877a7
thetribalme <thetribaleng@gmail.com>
2023-05-26 13:28:00 +0200

    task done (3/4)

--- Lesson_2/Lesson_2.4.py
+++ Lesson_2/Lesson_2.4.py
@@ -6,3 +6,19 @@
 экран фразы вида: 'Привет, Игорь!' Подумать, как получить имена сотрудников из элементов
 списка, как привести их к корректному виду. Можно ли при этом не создавать новый список?
 '''
+job_name_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй',
+                 'директор аэлита']
+
+# forming a new list
+name_list = []
+
+for i, v in enumerate(job_name_list):
+    internal_list = v.split(' ')
+    name_list.append(internal_list.pop())
+    print(f'Привет, {name_list[i].capitalize()}!')
+
+# without forming a new list
+for i, v in enumerate(job_name_list):
+    internal_list = v.split(' ')
+    job_name_list[i] = internal_list.pop()
+    print(f'Привет, {job_name_list[i].capitalize()}!')

281b3db7cb7caab3f0ef52eafb60b13f4f99451e
thetribalme <thetribaleng@gmail.com>
2023-05-28 17:23:11 +0200

    task done (1/4)

--- Lesson_2/Lesson_2.1.py
+++ Lesson_2/Lesson_2.1.py
@@ -5,3 +5,13 @@
 ● 15 // 2
 ● 15 ** 2
 '''
+
+operation_1 = 15 * 3
+operation_2 = 15 / 3
+operation_3 = 15 // 2
+operation_4 = 15 ** 2
+
+print(type(operation_1))
+print(type(operation_2))
+print(type(operation_3))
+print(type(operation_4))

752053a291666367cc2dfef114ff9e2151483ab2
thetribalme <thetribaleng@gmail.com>
2023-05-31 18:26:45 +0200

    tasks is done (1/3)

--- Lesson_3/Lesson_3.1-2.py
+++ Lesson_3/Lesson_3.1-2.py
@@ -8,10 +8,77 @@ num_translate("eight")
 Если перевод сделать невозможно, вернуть None. Подумайте, как и где лучше хранить информацию, необходимую для
 перевода: какой тип данных выбрать, в теле функции или снаружи.
 
-Задание 2. Оработать предыдущую функцию в num_translate_adv(): реализовать корректную работу с числительными,
+Задание 2. Обработать предыдущую функцию в num_translate_adv(): реализовать корректную работу с числительными,
 начинающимися с заглавной буквы — результат тоже должен быть с заглавной. Например:
 num_translate_adv("One")
 "Один"
 num_translate_adv("two")
 "два"
 '''
+# task 1
+print('FIRST PART'.center(100, '-'), '\n')
+
+
+def num_translate(number):
+    """
+    Translates english numbers from 0 to 10 into russian
+
+    :param number: number you want to translate. ONLY word form
+    :return: printed translation
+    """
+    translations = {
+        'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'
+    }
+    if number in translations.keys():
+        print(translations[number])
+    else:
+        print('None')
+
+
+num_translate('zero')
+num_translate('eleven')
+
+# task 2
+print('\n', 'SECOND PART'.center(100, '-'))
+
+
+def num_translate_adv(number):
+    translations = {
+        'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'
+    }
+    if number.lower() in translations.keys():
+        if number.istitle():
+            number = number.lower()
+            print(translations[number].capitalize())
+        elif number.islower():
+            print(translations[number])
+        else:
+            print('None')
+    else:
+        print('None')
+
+
+num_translate_adv('one')
+num_translate_adv('Two')
+num_translate_adv('tHree')
+num_translate_adv('twelve')

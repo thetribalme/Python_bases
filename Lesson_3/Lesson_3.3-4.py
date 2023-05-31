f9e087df386cbc7ab8d9bb83afe80641a91b7085
thetribalme <thetribaleng@gmail.com>
2023-05-31 20:05:19 +0200

    task is done (2/3)

--- Lesson_3/Lesson_3.3-4.py
+++ Lesson_3/Lesson_3.3-4.py
@@ -27,3 +27,56 @@
 }
 }
 '''
+# task 3
+# print('FIRST PART'.center(100, '-'), '\n')
+#
+#
+# def thesaurus(*names):
+#     """Returns dictionary with first name letters as keys and lists of names as values"""
+#     result = {}
+#     for name in names:
+#         key = name[0].capitalize()
+#         if key not in result:
+#             result[key] = []
+#             result[key].append(name)
+#         else:
+#             result[key].append(name)
+#     return result
+#
+#
+# print(thesaurus('Bethany', 'Ian', 'Theodor', 'Dora', 'Bob', 'Daniel'))
+#
+# # task 4
+# print('\n', 'SECOND PART'.center(100, '-'))
+#
+
+
+def thesaurus_adv(*personal_data):
+    """Returns dictionary with first surname letter as a key and a (dictionary with first name letter as a keys and
+    list of names and surnames as a value) as a value"""
+    result = {}
+    for name_surname in personal_data:
+        name = name_surname.split(' ')[0]
+        surname = name_surname.split(' ')[1]
+        first_key = surname[0].capitalize()
+        second_key = name[0].capitalize()
+        if first_key not in result:
+            result[first_key] = {}
+            name_based = result[first_key]
+            if second_key not in name_based:
+                name_based[second_key] = []
+                name_based[second_key].append(name_surname)
+            else:
+                name_based[second_key].append(name_surname)
+        else:
+            name_based = result[first_key]
+            if second_key not in name_based:
+                name_based[second_key] = []
+                name_based[second_key].append(name_surname)
+            else:
+                name_based[second_key].append(name_surname)
+    return result
+
+
+print(thesaurus_adv('Bethany Sommers', 'Ian Valensis', 'Theodor Samsonite', 'Dora Explorer', 'Bob Sackland',
+                    'Daniel Fufelshmertz'))

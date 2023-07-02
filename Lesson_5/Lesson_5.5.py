f467688fae6921ba43efcec7abaf043a93ebf2f1
thetribalme <thetribaleng@gmail.com>
2023-07-02 15:39:44 +0200

    task is done (4/4)

--- Lesson_5/Lesson_5.5.py
+++ Lesson_5/Lesson_5.5.py
@@ -4,3 +4,7 @@
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 result = [23, 1, 3, 10, 4, 11]
 '''
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+
+result = [element for element in src if src.count(element) == 1]
+print(result)

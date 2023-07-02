62746262068c42d6d80da8e3512031f10087b6ed
thetribalme <thetribaleng@gmail.com>
2023-07-02 15:32:46 +0200

    task is done (3/4)

--- Lesson_5/Lesson_5.4.py
+++ Lesson_5/Lesson_5.4.py
@@ -3,3 +3,7 @@
 src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
 result = [12, 44, 4, 10, 78, 123]
 '''
+src = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+result = [src[i] for i in range(1, len(src)) if src[i] > src[i - 1]]
+print(result)

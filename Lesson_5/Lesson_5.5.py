620b3c8509d42d2b3b9cef5479d8f12be36f6904
thetribalme <thetribaleng@gmail.com>
2023-07-05 20:26:52 +0200

    added another way to solve the task through sets (4/4)

--- Lesson_5/Lesson_5.5.py
+++ Lesson_5/Lesson_5.5.py
@@ -6,5 +6,18 @@ result = [23, 1, 3, 10, 4, 11]
 '''
 src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
 
+# for little number of list elements
 result = [element for element in src if src.count(element) == 1]
+print(result)
+
+# for big number of list elements
+unique_data = set()
+repeats = set()
+for element in src:
+    if element not in repeats:
+        unique_data.add(element)
+    else:
+        unique_data.discard(element)
+    repeats.add(element)
+result = [element for element in src if element in unique_data]
 print(result)

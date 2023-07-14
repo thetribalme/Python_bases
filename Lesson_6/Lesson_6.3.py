4f87fb5154f24a80cbe2dfc56f1131be6ebc4d1e
thetribalme <thetribaleng@gmail.com>
2023-07-14 15:50:17 +0200

    added dictionary comprehension (2/5)

--- Lesson_6/Lesson_6.3.py
+++ Lesson_6/Lesson_6.3.py
@@ -7,27 +7,20 @@
 При решении задачи считать, что объём данных в файлах во много раз меньше объема ОЗУ
 '''
 import json
-
-names_and_hobbies = {}
+from itertools import zip_longest
 
 with open('users.csv', 'r', encoding='utf-8') as users:
-    with open('hobby.csv', 'r', encoding='utf-8') as activities:
-        names = users.read().splitlines()
-        hobbies = activities.read().splitlines()
+    with open('hobby.csv', 'r', encoding='utf-8') as hobbies:
+        all_info_list = zip_longest(users, hobbies, fillvalue=None)
+        names_and_hobbies = {str(element[0].strip()): str(element[1].strip()) for element in all_info_list}
 
-if len(names) < len(hobbies):
+
+if 'None' in names_and_hobbies:
     exit(1)
 else:
-    for i in range(len(names)):
-        try:
-            names_and_hobbies[names[i]] = hobbies[i]
-        except IndexError:
-            names_and_hobbies[names[i]] = None
-
-# writing file in .txt
-with open('combined_file.txt', 'w', encoding='utf-8') as result_txt:
-    result_txt.writelines(str(names_and_hobbies))
-
-# writing file in .json
-with open('combined_file.json', 'w', encoding='utf-8') as result_json:
-    json.dump(names_and_hobbies, result_json, ensure_ascii=False, indent=4)
+    # writing file in .txt
+    with open('combined_file.txt', 'w', encoding='utf-8') as result_txt:
+        result_txt.writelines(str(names_and_hobbies))
+    # writing file in .json
+    with open('combined_file.json', 'w', encoding='utf-8') as result_json:
+        json.dump(names_and_hobbies, result_json, ensure_ascii=False, indent=4)
